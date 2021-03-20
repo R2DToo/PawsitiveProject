7a5cb50b61d1d1a97611aafef9df2b3526ccb2d7
Braden Still-Routley <Bradenxd12@gmail.com>
2021-03-20 18:32:27 -0500

    Voting Fragment

---
+++ app/src/main/java/com/example/pawsitiveproject/Vote.java
@@ -0,0 +1,114 @@
+package com.example.pawsitiveproject;
+
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Vote extends Object implements Comparable<Vote> {
+    private String country_code;
+    private String created_at;
+    private int id;
+    private String image_id;
+    private String sub_id;
+    private int value;
+    private String image_url;
+
+    public void Vote () {}
+
+    public Vote(String country_code, String created_at, int id, String image_id, String sub_id, int value) {
+        this.country_code = country_code;
+        this.created_at = created_at;
+        this.id = id;
+        this.image_id = image_id;
+        this.sub_id = sub_id;
+        this.value = value;
+    }
+
+    public String getCountry_code() {
+        return country_code;
+    }
+
+    public void setCountry_code(String country_code) {
+        this.country_code = country_code;
+    }
+
+    public Date getCreated_at() {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.CANADA);
+        dateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date_created = null;
+        try {
+            date_created = dateFormatter.parse(created_at);
+        } catch (ParseException pe) {
+            Log.d("bsr", "DATE PARSE ERROR: " + pe);
+        }
+        return date_created;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getSub_id() {
+        return sub_id;
+    }
+
+    public void setSub_id(String sub_id) {
+        this.sub_id = sub_id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "country_code='" + country_code + '\'' +
+                ", created_at='" + created_at + '\'' +
+                ", id=" + id +
+                ", image_id='" + image_id + '\'' +
+                ", sub_id='" + sub_id + '\'' +
+                ", value=" + value +
+                ", image_url='" + image_url + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Vote vote) {
+        if (getCreated_at() == null || vote.getCreated_at() == null)
+            return 0;
+        return getCreated_at().compareTo(vote.getCreated_at());
+    }
+}
--- app/src/main/java/com/example/pawsitiveproject/VotesFragment.java
+++ app/src/main/java/com/example/pawsitiveproject/VotesFragment.java
@@ -1,19 +1,197 @@
 package com.example.pawsitiveproject;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VotesFragment extends Fragment {
+    private final String API_KEY = "2fc6b7da-314a-4327-9888-0fd52d813f7f";
+    private final String TAG = "api-req";
+
+    private ArrayList<Vote> all_votes;
+    private VoteAdapter voteAdapter;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
         return inflater.inflate(R.layout.fragment_votes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        all_votes = new ArrayList<Vote>();
+        voteAdapter = new VoteAdapter(this.getContext(), R.layout.vote_list_item, all_votes);
+        ListView vote_list = view.findViewById(R.id.vote_list);
+        vote_list.setAdapter(voteAdapter);
+        getAllVotes();
+    }
+
+    private void getAllVotes() {
+        String url = "https://api.thedogapi.com/v1/votes?sub_id="+ currentUser.getUid();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url,
+            response -> {
+                Log.d("bsr", String.valueOf(response.length()));
+                for (int i = 0; i < response.length(); i++) {
+                    Gson gson = new Gson();
+                    JSONObject jsonObject = null;
+                    String string = null;
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        string = jsonObject.toString();
+                    } catch (JSONException je) {
+                        Log.e("bsr", "JSON ERROR: " + je);
+                    }
+                    Vote newVote = gson.fromJson(string, Vote.class);
+                    Log.d("bsr", i + ": vote == " + newVote.toString());
+                    if (i == response.length() - 1) {
+                        getImageURL(newVote, true);
+                    } else {
+                        getImageURL(newVote, false);
+                    }
+                }
+                Log.d("bsr", "Vote Loop Over");
+            },
+            error -> {
+                Log.d("bsr", "RESPONSE ERROR: " + error.toString());
+            }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("x-api-key", API_KEY);
+                return params;
+            }
+        };
+        jsonArrayRequest.setTag(TAG);
+        SharedRequestQueue.getInstance(this.getContext()).addToRequestQueue(jsonArrayRequest);
+    }
+
+    private void getImageURL(Vote newVote, boolean lastVote) {
+        String url = "https://api.thedogapi.com/v1/images/" + newVote.getImage_id();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, (Response.Listener<JSONObject>) response -> {
+            Gson gson = new Gson();
+            String string = response.toString();
+            RandomImageResponse imageResponse = gson.fromJson(string, RandomImageResponse.class);
+            newVote.setImage_url(imageResponse.getUrl());
+            all_votes.add(newVote);
+            Log.d("bsr", "Image URL == " + newVote.getImage_url());
+            if (lastVote == true) {
+                sortVotes();
+                voteAdapter.notifyDataSetChanged();
+            }
+        }, error -> {
+            Log.d("bsr", "RESPONSE ERROR: " + error.toString());
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("x-api-key", API_KEY);
+                return params;
+            }
+        };
+
+        jsonObjectRequest.setTag(TAG);
+        SharedRequestQueue.getInstance(this.getContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private void sortVotes() {
+        Collections.sort(all_votes, new Comparator<Vote>() {
+            @Override
+            public int compare(Vote vote, Vote v1) {
+                return vote.getCreated_at().compareTo(v1.getCreated_at());
+            }
+        });
+        Collections.reverse(all_votes);
+    }
+
+    private class VoteAdapter extends ArrayAdapter<Vote> {
+        private ArrayList<Vote> votes;
+
+        public VoteAdapter(Context context, int textViewResourceId, ArrayList<Vote> votes) {
+            super(context, textViewResourceId, votes);
+            this.votes = votes;
+        }
+
+        @Override
+        public int getCount() { return votes.size(); }
+
+        @Nullable
+        @Override
+        public Vote getItem(int position) { return votes.get(position); }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View view = convertView;
+
+            if (view == null) {
+                LayoutInflater viewInflater = (LayoutInflater)getLayoutInflater();
+                view = viewInflater.inflate(R.layout.vote_list_item, null);
+            }
+            Vote thisVote = getItem(position);
+            if (thisVote != null) {
+                LinearLayout vote_layout = view.findViewById(R.id.vote_layout);
+                if (thisVote.getValue() == 1) {
+                    vote_layout.setBackgroundColor(Color.parseColor("#7ff0a6"));
+                } else {
+                    vote_layout.setBackgroundColor(Color.parseColor("#e37f7f"));
+                }
+                ImageView img_vote_dog = view.findViewById(R.id.img_vote_dog);
+                Glide.with(this.getContext())
+                        .load(thisVote.getImage_url())
+                        .centerCrop()
+                        .placeholder(R.drawable.ic_baseline_cached_24)
+                        .into(img_vote_dog);
+                TextView vote_id = view.findViewById(R.id.vote_id);
+                vote_id.setText(String.valueOf(thisVote.getId()));
+                TextView created_at = view.findViewById(R.id.vote_created_at);
+                created_at.setText(thisVote.getCreated_at().toString());
+                TextView vote_image_id = view.findViewById(R.id.vote_image_id);
+                vote_image_id.setText(thisVote.getImage_id());
+            }
+            return view;
+        }
     }
 }
